b3f8c0f757a8580e40b3cafb0b898248e02a1982
Dominique Dierickx <d.dierickx@gmail.com>
2012-01-06 14:50:28 +0100

    Cleaned up

--- foursquarestats/fetch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf out.csv
-scp tahadoop@10.1.6.16:domi/scripts/foursquarestats/out.csv out.csv
--- foursquarestats/run_colruyt.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python fsquerystats.py colruyt 15 colruyt_data/token.txt colruyt_data/colruyt_locations.txt colruyt_data/queryresults.csv
